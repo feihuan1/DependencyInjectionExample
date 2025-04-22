d2a0b6d4c320e3f14ead1b40b52c0f9f395a180d
Feihuan Peng <feihuan.peng@spreetail.com>
2025-04-21 23:55:02 -0500

    Add project files.

---
+++ DependencyInjectionExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace DependencyInjectionExample.Controllers
+{
+    // dependency injection -> add service in program.cs
+    public class HomeController : Controller
+    {
+        // construction injection make it avaliable for all actions
+        //private readonly ICitiesService _citiesSevice;
+        //public HomeController(ICitiesService citiesService)
+        //{
+        //    // instantiate citiesService
+        //    _citiesSevice = citiesService; //new CitiesService();
+        //}
+
+        [Route("/")]
+        public IActionResult Index([FromServices] ICitiesService citiesSevice)// method injection make it avalible only for this action
+        {
+            List<string> cities = citiesSevice.GetCities();
+            return View(cities);
+        }
+    }
+}
---
+++ Services/CitiesService.cs
@@ -0,0 +1,28 @@
+﻿using ServiceContracts;
+
+namespace Services
+{
+    public class CitiesService : ICitiesService
+    {
+        private List<string> _cities = new List<string>();
+        // from db
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Tokyo",
+                "Rome"
+            };
+        }
+
+        // fetching database
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+    }
+}
